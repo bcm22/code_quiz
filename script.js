371c148a8f5c08b83964009bfb924b833c420e88
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-07-11 13:05:31 -0500

    Added Functions to power quiz

--- script.js
+++ script.js
@@ -4,29 +4,102 @@ var answerButton1 = document.querySelector("#answer1");
 var answerButton2 = document.querySelector("#answer2");
 var answerButton3 = document.querySelector("#answer3");
 var answerButton4 = document.querySelector("#answer4");
-var scores=[];
+var answerButtons = document.querySelectorAll(".answer-button")
+
+var startButton = document.querySelector(".start-button");
+var questionDiv = document.querySelector(".question-div")
+var introDirv = document.querySelector(".intro")
+var timerElement = document.querySelector(".timer");
+var scores = [];
+var timerCount;
+var timer;
+var questionNumber;
+
+const QUESTIONS = [
+    "Commonly used data types DO NOT inlcude",
+    "QUESTION @",
+    "QUESTION 3"
+]
+
+const ANSWERS1 = [
+    "strings"
+]
+
+const ANSWERS2 = [
+    "booleans"
+]
+
+const ANSWERS3 = [
+    "alerts"
+]
+
+const ANSWERS4 = [
+    "numbers"
+]
 
 
-function question1(){
-    question.textContent="The first question"
-    answerButton1.textContent="First "
+function createQuestion(number) {
+    questionNumber = number
+    question.textContent = QUESTIONS[questionNumber - 1]
+    answerButton1.textContent = ANSWERS1[questionNumber - 1]
+    answerButton2.textContent = ANSWERS2[questionNumber - 1]
+    answerButton3.textContent = ANSWERS3[questionNumber - 1]
+    answerButton4.textContent = ANSWERS4[questionNumber - 1]
+    questionDiv.classList.remove("display-none")
+    nextQuestion = false
 }
 
-function question2(){
-
+function startQuiz() {
+    introDirv.classList.add("display-none")
+    timerCount = 60;
+    createQuestion(1)
+    startTimer()
 }
 
-function startQuiz(){
-    question1()
-
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = "Time: " + timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                //   winGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            // loseGame();
+        }
+    }, 1000);
 }
 
-function checkAnswer(questionNumber, answer){
-    if(questionNumber == 1){
-        // return answer ==
-    }
-}
 
-function clearScores(){
+function clearScores() {
     scores = []
+}
+
+startButton.addEventListener("click", startQuiz);
+for (i of answerButtons) {
+    i.addEventListener('click', function () {
+        const answer = this.textContent
+        console.log("CREATED LISTENER", answer, questionNumber)
+        if (questionNumber == 1) {
+            if (answer === 'alerts') {
+                //correct
+                console.log("CORRECT")
+            }
+            else {
+                //incorrect
+                timerCount -= 10
+                console.log("INCORRECT")
+            }
+            createQuestion(2)
+        }
+    });
 }
